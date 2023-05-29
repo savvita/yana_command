308f69e76978f8c2a30177e761be50815869683b
thespiker <Fili_vw23@student.itstep.org>
2023-05-29 20:44:07 +0300

    init back

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PixabayApi.Helpers;
+
+namespace PixabayApi.Controllers {
+
+    [ApiController]
+    [Route("api")]
+    public class ImageController :ControllerBase{
+
+        [HttpGet(Name = "getImages")]
+        
+        public async Task<IActionResult> getImages(string text) {
+            return Ok(await RequestHelper.GetAsync($"https://pixabay.com/api/?key=29800629-4ce57f3dcb86337a1cd80b83b&q={text}&image_type=photo&per_page=3"));
+        }
+    }
+}
---
+++ Helpers/RequestHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace PixabayApi.Helpers {
+    public class RequestHelper {
+
+        public static async Task<string> GetAsync(string uri) {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream)) {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
