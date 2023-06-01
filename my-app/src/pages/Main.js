015f3e0f5ee8f981a9fa10ebf39e98aa9d5dd4c8
thespiker <Fili_vw23@student.itstep.org>
2023-06-01 13:01:08 +0300

    fix delete button

--- my-app/src/pages/Main.js
+++ my-app/src/pages/Main.js
@@ -8,25 +8,22 @@ import axios from "axios";
 import { Hosting } from '../components/Hosting.ts';
 
 function Main() {
-    // let obj = {"total":0,"totalHits":0,"hits":[]};
-    // let obj2 = {"total":1954,"totalHits":500,"hits":[{"id":3386356,"pageURL":"https://pixabay.com/photos/piglet-mammal-species-fauna-3386356/","type":"photo","tags":"piglet, mammal, species","previewURL":"https://cdn.pixabay.com/photo/2018/05/09/22/44/piglet-3386356_150.jpg","previewWidth":150,"previewHeight":99,"webformatURL":"https://pixabay.com/get/g559a0719535d311c566d451e688a266be9cd5eb79181f69ae9b8a23980fa65b85791056a7ba710f07af034e8cabbd60a7bcac780e5dec332b502dad8f69197e1_640.jpg","webformatWidth":640,"webformatHeight":426,"largeImageURL":"https://pixabay.com/get/g7cc7554c615e04109808008e4e4020b9ecae94aa679adff08908b38c942e488c9946e6e4c8f58d9674ab059276fe072e19f63bf79af2aa375d7ef278d2b34475_1280.jpg","imageWidth":4851,"imageHeight":3234,"imageSize":3682743,"views":131796,"downloads":70377,"collections":398,"likes":439,"comments":123,"user_id":3128024,"user":"RoyBuri","userImageURL":"https://cdn.pixabay.com/user/2017/10/30/22-28-19-779_250x250.png"},{"id":242520,"pageURL":"https://pixabay.com/photos/guinea-pig-cavy-pet-guinea-rodent-242520/","type":"photo","tags":"guinea pig, cavy, pet","previewURL":"https://cdn.pixabay.com/photo/2014/01/11/23/40/guinea-pig-242520_150.jpg","previewWidth":150,"previewHeight":99,"webformatURL":"https://pixabay.com/get/g7a4ff5aa08099c4f7c6810f650d33cc9dec0298174f5c8260e0b13a7f9e1cc6f113f41584fd2a2b87eca50bbb9c90235_640.jpg","webformatWidth":640,"webformatHeight":425,"largeImageURL":"https://pixabay.com/get/g760a5346b7b469bbf8a4de22c5ed342e1f9a5ee10d92f1357620b558a312e3cee350bea4e02b4bcf1cec2dba388cde8c10f26361c2ce48da33ab3c7ab4bfacfd_1280.jpg","imageWidth":3008,"imageHeight":2000,"imageSize":966620,"views":105128,"downloads":37314,"collections":268,"likes":226,"comments":34,"user_id":55562,"user":"vantagepointfl","userImageURL":""},{"id":3741880,"pageURL":"https://pixabay.com/photos/piglets-animal-habitat-species-pig-3741880/","type":"photo","tags":"piglets, animal, habitat","previewURL":"https://cdn.pixabay.com/photo/2018/10/12/10/31/piglets-3741880_150.jpg","previewWidth":150,"previewHeight":100,"webformatURL":"https://pixabay.com/get/g0091f2412c34d3448814ea253e4d34dbf55a1fc7707b4c8731cc387d062302bd9aa0dc11d12811708947ea52e3b453d37de7b2639ec3f9d7a67a886771df28d0_640.jpg","webformatWidth":640,"webformatHeight":427,"largeImageURL":"https://pixabay.com/get/g3351f0555bd27475f474dd9d49bd240b53fce39518b354ba104e105db601fb9b181749ecc2c8757c525613185245d192742a06b04352cd2dbe752e5cf6059017_1280.jpg","imageWidth":4900,"imageHeight":3271,"imageSize":4369135,"views":62223,"downloads":50052,"collections":276,"likes":142,"comments":77,"user_id":1377835,"user":"MabelAmber","userImageURL":"https://cdn.pixabay.com/user/2023/04/19/11-42-04-154_250x250.jpg"}]};
     const [host] = useState(new Hosting());
     const [search, setSearch] = useState("");
     const [pix, setPix] = useState([]);
     const [isHiddenPicture, setHiddenPicture] = useState("none");
     const [isHiddenCards, setHiddenCards] = useState("none");
-    const [numberOfPaage,setNumberOfPaage] = useState(2);
+    const [numberOfPaage,setNumberOfPaage] = useState(3);
     const[pictures,setPictures] = useState([]);
     const [tempPicture,setTempPicture] = useState([]);
-    // const [end,setEnd] = useState(false)
+
     function getPixByName(name) {
-        // setPix(obj2.hits.map((item, index) => (item.largeImageURL)));
-        // return;
+
         if (name != "") {
             axios({
                 method: 'get',
-                url: `https://pixabay.com/api/?key=29800629-4ce57f3dcb86337a1cd80b83b&q=${name}&image_type=photo&per_page=3`,
-                // url: `https://${host.getHost()}/getImages?text=${name}`,
+                //url: `https://pixabay.com/api/?key=29800629-4ce57f3dcb86337a1cd80b83b&q=${name}&image_type=photo&per_page=3`,
+                 url: `https://${host.getHost()}/getImages?text=${name}`,
                 headers: {
                     'Accept': '*/*',
                     'Content-Type': 'application/json'
@@ -39,7 +36,7 @@ function Main() {
                     }
                     else {
                         setPix(data.data.hits.map((item, index) => (item.largeImageURL)));
-                        setNumberOfPaage(2);
+                        setNumberOfPaage(3);
                         setCache(name);
                         setHiddenCards("flex");
                         setHiddenPicture("none");
@@ -56,63 +53,42 @@ function Main() {
     function setCache(text){
         axios({
             method: 'get',
-            url: `https://pixabay.com/api/?key=29800629-4ce57f3dcb86337a1cd80b83b&q=${text}&image_type=photo&per_page=3&page=${numberOfPaage}"`,
+            //url: `https://pixabay.com/api/?key=29800629-4ce57f3dcb86337a1cd80b83b&q=${text}&image_type=photo&per_page=3&page=3"`,
+            url: `https://${host.getHost()}/getImagesPaginate?text=${text}&perPage=3&page=${numberOfPaage}`,
             headers: {
                 'Accept': '*/*',
                 'Content-Type': 'application/json'
             }
         }).then(res => {
             setPictures(res.data.hits);
-            console.log(pictures);
+            console.log(numberOfPaage);
+            console.log(res.data.hits);
         })
-        axios({
-            method: 'get',
-            url: `https://pixabay.com/api/?key=29800629-4ce57f3dcb86337a1cd80b83b&q=${text}&image_type=photo&per_page=100`,
-            // url: `https://${host.getHost()}/getImages?text=${name}`,
-            headers: {
-                'Accept': '*/*',
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(result=>{
-            setTempPicture(result.data.hits)
-            console.log(tempPicture);
-        })
+    
     }
-    function deleteCard(event){
-        var mas = [];
-        let end = false
+    function deleteCardTest(event){
+        if(pictures.length==0){
+            setNumberOfPaage(numberOfPaage+1);
+            setCache(search);
+            
+        }else{
+            changePicture(event);
+        }
+        
+    }
+    function changePicture(e){
+        let mas =[]
         for(let i =0; i< pix.length; i ++){
-            if(i != event.target.id){
+            if(i != e.target.id){
                 mas.push(pix[i])
-            }
-            else{
-                do{
-                    const ind = Math.round(Math.random() * (98 - 3))
-                    console.log(tempPicture[ind]['previewURL']);
-                    const flag = 0;
-                    for(let x =0; x< pix.length; x++){
-                        if(pix[x] == tempPicture[ind]['previewURL']){
-                            flag = flag + 1;
-                        }
-                    }
-                    if(flag == 0){
-                        console.log("flag");
-                        mas.push(tempPicture[ind]['previewURL'])
-                        console.log(mas);
-                        end = true
-                        break
-                        // setEnd(true)
-                        // return
-                    }
-                }
-                while(!end)
-                console.log("end do while");
+            }else{
+                mas.push(pictures[0]['largeImageURL']);
+                pictures.shift();
             }
         }
-        setPix(mas)
-        console.log(pix);
+        setPix(mas);
     }
+
     return (
         <div>
             <div id="form-search" action="">
@@ -131,7 +107,7 @@ function Main() {
             <br></br>
             <div id="form-pix" style={{ display: isHiddenCards }}>
                 <Cards
-                    deleteCard={deleteCard}
+                    deleteCard={deleteCardTest}
                     pix={pix}
                 ></Cards>
             </div>
