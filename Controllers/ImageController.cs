0e6ade7580267f6f3c7680eae4b28856e038052b
Davud1Ramazanov <Rama_bz54@student.itstep.org>
2023-06-01 20:10:21 +0300

    Create find images by user name

--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Mvc;
 using PixabayApi.Helpers;
 
 namespace PixabayApi.Controllers {
 
     [ApiController]
-    [Route("api")]
+    [Route("api/[controller]")]
     public class ImageController :ControllerBase{
 
         private readonly IConfiguration configuration;
@@ -14,9 +15,16 @@ namespace PixabayApi.Controllers {
             this.configuration = configuration;
         }
 
-        [HttpGet(Name = "getImages")]
+        [HttpGet]
+        [Route("getImages")]
         public async Task<IActionResult> getImages(string text) {
             return Ok(await RequestHelper.GetAsync($"https://pixabay.com/api/?key={configuration["Authorization:Key"]}&q={text}&image_type=photo&per_page=3"));
+        }
+
+        [HttpGet]
+        [Route("getUserImages")]
+        public async Task<IActionResult> getUserImages(string text) {
+            return Ok(await RequestHelper.GetAsync($"https://pixabay.com/api/?key={configuration["Authorization:Key"]}&q=user:{text}"));
         }
     }
 }
